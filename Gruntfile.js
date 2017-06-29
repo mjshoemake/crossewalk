ac51fc15fb1f3ffcce020eb8142530bf27c3c925
Mike Shoemake <Dad@SHOEMAKE-MAC-MINI.local>
2017-06-28 23:59:18 -0400

    Fixed a minor style bug on the referrals page.

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
         },
         files: {
           // destination file and source file       
-          "src/main/webapp/assets/css/theme2.0.1.css": "less/theme2.less", 
+          "src/main/webapp/assets/css/theme2.0.2.css": "less/theme2.less", 
           "src/main/webapp/assets/css/03-headroom.css": "less/headroom.less" 
         }
       }
