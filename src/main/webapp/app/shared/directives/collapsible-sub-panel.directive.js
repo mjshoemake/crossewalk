a00f52887d37b194526be4425d940525e21661d0
Mike Shoemake <Dad@SHOEMAKE-MAC-MINI.local>
2017-07-05 00:37:22 -0400

    Trying to fix the bug with directive interfering with collapsing accordion.

--- src/main/webapp/app/shared/directives/collapsible-sub-panel.directive.js
+++ src/main/webapp/app/shared/directives/collapsible-sub-panel.directive.js
@@ -1,27 +1,27 @@
 angular.module('app.shared-directives').directive("collapsibleSubPanel", function() {
     return {
-        restrict: 'E',
+        restrict: 'A',
         link: function(scope, element, attrs, ctrl, $transclude) {
             scope.pretitle = attrs.pretitle;
             scope.boldtitle = attrs.boldtitle;
             scope.posttitle = attrs.posttitle;
             scope.id = attrs.id;
-            scope.class = attrs.class;
-            scope.panelstyle = attrs.panelstyle;
+            scope.collapseclass = attrs.collapseclass;
+//            scope.panelstyle = attrs.panelstyle;
         },
 		scope: {},
         transclude: true,
         template:
-            '				<div class="panel panel-default subsection" style="{{panelstyle}}">' +
+//            '				<div class="panel panel-default subsection" style="{{panelstyle}}">' +
             '					<div class="panel-heading">' +
             '						<a data-toggle="collapse" target="_self" data-parent="#accordion" href="#collapse-{{id}}">' +
             '							{{pretitle}}<b>{{boldtitle}}</b>{{posttitle}}' +
             '						</a>' +
             '					</div>' +
-            '					<div id="collapse-{{id}}" class="panel-collapse {{class}}">' +
+            '					<div id="collapse-{{id}}" class="panel-collapse {{collapseclass}}">' +
             '						<div class="panel-body" ng-transclude>' +
             '						</div>' +
-            '					</div>' +
-            '				</div>' 
+            '					</div>' 
+//            '				</div>' 
     }
 })
